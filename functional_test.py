ea90feb9fbe65dd4e3329c2541556f98ae41e6ae
hgsong <hgsong@wantedlab.com>
2018-12-19 12:19:08 +0900

    add first functional test comment and use unittest

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,31 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome('chromedriver')
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome('chromedriver')
+        self.browser.implicitly_wait(3)
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+       
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+# She add task
+# Whe input 'bye feather'
+# When she hit enter key, '1: bye feather' item is added
+# There is extra input text field and add 'make web with feather'
+# Page is reloaded and there are two items
+# Edis is wondering if the web site save items
+# Site make URL for her
+# description for URL is provieded together.
+
+# She can check if the list exists.
+# She fall a sleep with satisfaction
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
