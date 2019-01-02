e4b883f1dabedfdc8f270f41ce5d27c0e3ac9572
hgsong <hgsong@wantedlab.com>
2019-01-02 12:25:35 +0900

    check for input item in functional test

--- functional_test.py
+++ functional_test.py
@@ -13,11 +13,29 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
        
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'task item input'
+        )
+
+        # She add task
+        # Whe input 'bye feather'
+        inputbox.send_keys('buy feathers')
+        inputbox.send_keys(Keys.ENTER)
+
+        # When she hit enter key, '1: bye feather' item is added
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+                any(row.text == '1: buy feathers' for row in rows),
+        )
+
         self.fail('Finish the test!')
 
-# She add task
-# Whe input 'bye feather'
-# When she hit enter key, '1: bye feather' item is added
 # There is extra input text field and add 'make web with feather'
 # Page is reloaded and there are two items
 # Edis is wondering if the web site save items
